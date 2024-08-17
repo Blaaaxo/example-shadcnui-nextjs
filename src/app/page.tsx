c3d3ea90b9ece999dc68a82289a303e6c2efd6f8
brayanfe2009 <brayanfe2009@gmail.com>
2024-08-17 00:50:36 +0000

    componentes de la libreria instalados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Example dashboard to Shadcn Ui library",
+}
 
 export default function Home() {
   return (
